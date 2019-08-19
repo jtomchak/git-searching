cd76a998ec5125bc70ff456820c619f7f9b3e3c9
Jesse Tomchak <jtomchak@gmail.com>
2019-08-19 11:51:38 -0700

    base testing and search controlled input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    searchTerm: "",
+    isLoading: false
+  };
+
+  handleSearchInputOnChange = event => {
+    console.log(event.target);
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
   render() {
+    const { searchTerm } = this.state;
+    // Set simple validation for searchterm length
+    const isEnabled = searchTerm.length > 2;
     return (
       <div className="columns">
         <div className="column is-half is-offset-one-quarter">
           <div className="field has-addons is-expanded">
-            <div className="control">
+            <div className="control is-expanded">
               <input
                 className="input is-large"
                 type="text"
-                placeholder="Find a repository"
+                placeholder="Find a User"
+                value={searchTerm}
+                onChange={this.handleSearchInputOnChange}
               />
             </div>
             <div className="control">
-              <a className="button is-info is-large">Search</a>
+              <button disabled={!isEnabled} className="button is-info is-large">
+                Search
+              </button>
             </div>
           </div>
         </div>
